797676a94e19aa670577a0ebd5e7905eff1e668c
Ian Martinez <ianmartinez97@live.com>
2018-12-04 19:16:49 -0600

    Add luminance and contrast functions, and fix parser bug

--- tamarack/tamarack.js
+++ tamarack/tamarack.js
@@ -351,6 +351,150 @@ class tkColor {
 		if(_color_string)
 			this.parse(_color_string);
 	}
+	
+	static get colorNames() {
+		return {"aliceblue":"#f0f8ff",
+		"antiquewhite":"#faebd7",
+		"aqua":"#00ffff",
+		"aquamarine":"#7fffd4",
+		"azure":"#f0ffff",
+		"beige":"#f5f5dc",
+		"bisque":"#ffe4c4",
+		"black":"#000000",
+		"blanchedalmond":"#ffebcd",
+		"blue":"#0000ff",
+		"blueviolet":"#8a2be2",
+		"brown":"#a52a2a",
+		"burlywood":"#deb887",
+		"cadetblue":"#5f9ea0",
+		"chartreuse":"#7fff00",
+		"chocolate":"#d2691e",
+		"coral":"#ff7f50",
+		"cornflowerblue":"#6495ed",
+		"cornsilk":"#fff8dc",
+		"crimson":"#dc143c",
+		"cyan":"#00ffff",
+		"darkblue":"#00008b",
+		"darkcyan":"#008b8b",
+		"darkgoldenrod":"#b8860b",
+		"darkgray":"#a9a9a9",
+		"darkgreen":"#006400",
+		"darkkhaki":"#bdb76b",
+		"darkmagenta":"#8b008b",
+		"darkolivegreen":"#556b2f",
+		"darkorange":"#ff8c00",
+		"darkorchid":"#9932cc",
+		"darkred":"#8b0000",
+		"darksalmon":"#e9967a",
+		"darkseagreen":"#8fbc8f",
+		"darkslateblue":"#483d8b",
+		"darkslategray":"#2f4f4f",
+		"darkturquoise":"#00ced1",
+		"darkviolet":"#9400d3",
+		"deeppink":"#ff1493",
+		"deepskyblue":"#00bfff",
+		"dimgray":"#696969",
+		"dodgerblue":"#1e90ff",
+		"firebrick":"#b22222",
+		"floralwhite":"#fffaf0",
+		"forestgreen":"#228b22",
+		"fuchsia":"#ff00ff",
+		"gainsboro":"#dcdcdc",
+		"ghostwhite":"#f8f8ff",
+		"gold":"#ffd700",
+		"goldenrod":"#daa520",
+		"gray":"#808080",
+		"green":"#008000",
+		"greenyellow":"#adff2f",
+		"honeydew":"#f0fff0",
+		"hotpink":"#ff69b4",
+		"indianred ":"#cd5c5c",
+		"indigo":"#4b0082",
+		"ivory":"#fffff0",
+		"khaki":"#f0e68c",
+		"lavender":"#e6e6fa",
+		"lavenderblush":"#fff0f5",
+		"lawngreen":"#7cfc00",
+		"lemonchiffon":"#fffacd",
+		"lightblue":"#add8e6",
+		"lightcoral":"#f08080",
+		"lightcyan":"#e0ffff",
+		"lightgoldenrodyellow":"#fafad2",
+		"lightgrey":"#d3d3d3",
+		"lightgreen":"#90ee90",
+		"lightpink":"#ffb6c1",
+		"lightsalmon":"#ffa07a",
+		"lightseagreen":"#20b2aa",
+		"lightskyblue":"#87cefa",
+		"lightslategray":"#778899",
+		"lightsteelblue":"#b0c4de",
+		"lightyellow":"#ffffe0",
+		"lime":"#00ff00",
+		"limegreen":"#32cd32",
+		"linen":"#faf0e6",
+		"magenta":"#ff00ff",
+		"maroon":"#800000",
+		"mediumaquamarine":"#66cdaa",
+		"mediumblue":"#0000cd",
+		"mediumorchid":"#ba55d3",
+		"mediumpurple":"#9370d8",
+		"mediumseagreen":"#3cb371",
+		"mediumslateblue":"#7b68ee",
+		"mediumspringgreen":"#00fa9a",
+		"mediumturquoise":"#48d1cc",
+		"mediumvioletred":"#c71585",
+		"midnightblue":"#191970",
+		"mintcream":"#f5fffa",
+		"mistyrose":"#ffe4e1",
+		"moccasin":"#ffe4b5",
+		"navajowhite":"#ffdead",
+		"navy":"#000080",
+		"oldlace":"#fdf5e6",
+		"olive":"#808000",
+		"olivedrab":"#6b8e23",
+		"orange":"#ffa500",
+		"orangered":"#ff4500",
+		"orchid":"#da70d6",
+		"palegoldenrod":"#eee8aa",
+		"palegreen":"#98fb98",
+		"paleturquoise":"#afeeee",
+		"palevioletred":"#d87093",
+		"papayawhip":"#ffefd5",
+		"peachpuff":"#ffdab9",
+		"peru":"#cd853f",
+		"pink":"#ffc0cb",
+		"plum":"#dda0dd",
+		"powderblue":"#b0e0e6",
+		"purple":"#800080",
+		"rebeccapurple":"#663399",
+		"red":"#ff0000",
+		"rosybrown":"#bc8f8f",
+		"royalblue":"#4169e1",
+		"saddlebrown":"#8b4513",
+		"salmon":"#fa8072",
+		"sandybrown":"#f4a460",
+		"seagreen":"#2e8b57",
+		"seashell":"#fff5ee",
+		"sienna":"#a0522d",
+		"silver":"#c0c0c0",
+		"skyblue":"#87ceeb",
+		"slateblue":"#6a5acd",
+		"slategray":"#708090",
+		"snow":"#fffafa",
+		"springgreen":"#00ff7f",
+		"steelblue":"#4682b4",
+		"tan":"#d2b48c",
+		"teal":"#008080",
+		"thistle":"#d8bfd8",
+		"tomato":"#ff6347",
+		"turquoise":"#40e0d0",
+		"violet":"#ee82ee",
+		"wheat":"#f5deb3",
+		"white":"#ffffff",
+		"whitesmoke":"#f5f5f5",
+		"yellow":"#ffff00",
+		"yellowgreen":"#9acd32"};
+	}
 
 	/**
 	 * Generate a random RGB color, without an alpha channel.
@@ -569,7 +713,9 @@ class tkColor {
 			var trimmed = _input.replace("hsla(","");
 			trimmed = trimmed.replace("hsl(","");
 			trimmed = trimmed.replace(")","");
-			var chunks = trimmed.split(",");
+			trimmed = trimmed.replace("'","");
+			trimmed = trimmed.replace("\"","");
+
 			if (chunks.length == 3) // hsl
 				this.fromHsla(parseInt(chunks[0]),parseInt(chunks[1]),parseInt(chunks[2]),1);
 			else if (chunks.length == 4) //hsla 
@@ -578,11 +724,15 @@ class tkColor {
 			var trimmed = _input.replace("rgba(","");
 			trimmed = trimmed.replace("rgb(","");
 			trimmed = trimmed.replace(")","");
+			trimmed = trimmed.replace("'","");
+			trimmed = trimmed.replace("\"","");
 			var chunks = trimmed.split(",");
+
 			if (chunks.length == 3) // rgb
-				this.fromRgba(parseInt(chunks[0]),parseInt(chunks[1]),parseInt(chunks[2]),1);
+				this.fromRgba(parseInt(chunks[0]), parseInt(chunks[1]), parseInt(chunks[2]), 1);
 			else if (chunks.length == 4) //rgba
-				this.fromRgba(parseInt(chunks[0]),parseInt(chunks[1]),parseInt(chunks[2]),parseFloat(chunks[3]),1);
+				this.fromRgba(parseInt(chunks[0]), parseInt(chunks[1]), parseInt(chunks[2]), parseFloat(chunks[3]), 1);
+
 		} else {
 			this.fromRgba(0,0,0,0);
 		}
@@ -744,12 +894,34 @@ class tkColor {
 	}
 
 	/**
-	 * If the current tkColor's lightness is less than 50% and the
-	 * alpha is more than 0.4.
+	 * Calculate the luminance of the color.
+	 * @returns {Number} the luminance
+	 */
+	getLuminance() {
+		var a = [this.r, this.g, this.b].map((v) => {
+			v /= 255;
+			return v <= 0.03928
+				? v / 12.92
+				: Math.pow( (v + 0.055) / 1.055, 2.4 );
+		});
+
+		return (a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722) * this.a;
+	}
+
+	contrastWith(_color) {
+		return (_color.getLuminance() + 0.05) / (this.getLuminance() + 0.05);
+	}
+
+	/**
+	 * If the current tkColor:
+	 *  - Has a lightness less than or equal to 50% or a saturation less or equal to 0.85
+	 *  - A alpha is more or equal to 0.4
 	 * @returns {Boolean} True if dark, false otherwise
 	 */
 	isDark() {
-		return (this.l <= 50 && this.a > 0.4);
+		var white = new tkColor("rgb('255,255,255')");
+		
+		return this.contrastWith(white) > 1.5;
 	}
 	
 	/**
