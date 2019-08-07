8405ee3ea789ccddf5443fd331fa45e2f38b11ed
ianmartinez <ianmartinez97@live.com>
2019-08-07 18:57:47 -0500

    Add tk.color

--- src/tamarack.js
+++ src/tamarack.js
@@ -1,8 +1,14 @@
-
+/**
+ * Tamarack:
+ * A library for 
+ */
 
 
 // The tamarackjs "namespace" - tk, for short
 var tk = {};
+
+// Values
+tk.version = 0.5;
 
 // Enums
 tk.LayoutDirection = Object.freeze({
@@ -74,11 +80,32 @@ tk.Random = class {
 	}
 
 	static decimal(min, max) {
-		return Math.random() < 0.5 ? ((1-Math.random()) * (max-min) + min) : (Math.random() * (max-min) + min);
+		return Math.random() < 0.5 ? 
+			((1-Math.random()) * (max-min) + min) : (Math.random() * (max-min) + min);
 	}
 
 	static item(...array) {
 		return array[tk.Random.integer(0, array.length - 1)];
+	}
+
+	static rgbString() {
+		return "rgb(" + tk.Random.integer(0,255) + "," + tk.Random.integer(0,255) + "," + tk.Random.integer(0,255) + ")";
+	}
+
+	static rgbaString() {
+		return "rgba(" + tk.Random.integer(0,255) + "," + tk.Random.integer(0,255) + "," + tk.Random.integer(0,255) + "," + (Math.random()).toFixed(2) + ")";
+	}
+
+	static color() {
+		var color = new tk.Color();
+		color.randomize();
+		return color;
+	}
+
+	static opaqueColor() {
+		var color = new tk.Color();
+		color.randomizeOpaque();
+		return color;
 	}
 }
 
@@ -97,7 +124,301 @@ tk.Number = class {
 }
 
 tk.Color = class {
+	static _named = {"aliceblue":"#f0f8ff", "antiquewhite":"#faebd7", "aqua":"#00ffff", "aquamarine":"#7fffd4", "azure":"#f0ffff", 
+	"beige":"#f5f5dc", "bisque":"#ffe4c4", "black":"#000000", "blanchedalmond":"#ffebcd", "blue":"#0000ff", "blueviolet":"#8a2be2", 
+	"brown":"#a52a2a", "burlywood":"#deb887", "cadetblue":"#5f9ea0", "chartreuse":"#7fff00", "chocolate":"#d2691e", "coral":"#ff7f50", 
+	"cornflowerblue":"#6495ed", "cornsilk":"#fff8dc", "crimson":"#dc143c", "cyan":"#00ffff", "darkblue":"#00008b", "darkcyan":"#008b8b", 
+	"darkgoldenrod":"#b8860b", "darkgray":"#a9a9a9", "darkgreen":"#006400", "darkkhaki":"#bdb76b", "darkmagenta":"#8b008b", 
+	"darkolivegreen":"#556b2f", "darkorange":"#ff8c00", "darkorchid":"#9932cc", "darkred":"#8b0000", "darksalmon":"#e9967a", 
+	"darkseagreen":"#8fbc8f", "darkslateblue":"#483d8b", "darkslategray":"#2f4f4f", "darkturquoise":"#00ced1", "darkviolet":"#9400d3", 
+	"deeppink":"#ff1493", "deepskyblue":"#00bfff", "dimgray":"#696969", "dodgerblue":"#1e90ff", "firebrick":"#b22222", 
+	"floralwhite":"#fffaf0", "forestgreen":"#228b22", "fuchsia":"#ff00ff", "gainsboro":"#dcdcdc", "ghostwhite":"#f8f8ff", 
+	"gold":"#ffd700", "goldenrod":"#daa520", "gray":"#808080", "green":"#008000", "greenyellow":"#adff2f", "honeydew":"#f0fff0", 
+	"hotpink":"#ff69b4", "indianred ":"#cd5c5c", "indigo":"#4b0082", "ivory":"#fffff0", "khaki":"#f0e68c", "lavender":"#e6e6fa", 
+	"lavenderblush":"#fff0f5", "lawngreen":"#7cfc00", "lemonchiffon":"#fffacd", "lightblue":"#add8e6", "lightcoral":"#f08080", 
+	"lightcyan":"#e0ffff", "lightgoldenrodyellow":"#fafad2", "lightgrey":"#d3d3d3", "lightgreen":"#90ee90", "lightpink":"#ffb6c1", 
+	"lightsalmon":"#ffa07a", "lightseagreen":"#20b2aa", "lightskyblue":"#87cefa", "lightslategray":"#778899", "lightsteelblue":"#b0c4de", 
+	"lightyellow":"#ffffe0", "lime":"#00ff00", "limegreen":"#32cd32", "linen":"#faf0e6", "magenta":"#ff00ff", "maroon":"#800000", 
+	"mediumaquamarine":"#66cdaa", "mediumblue":"#0000cd", "mediumorchid":"#ba55d3", "mediumpurple":"#9370d8", "mediumseagreen":"#3cb371", 
+	"mediumslateblue":"#7b68ee", "mediumspringgreen":"#00fa9a", "mediumturquoise":"#48d1cc", "mediumvioletred":"#c71585", 
+	"midnightblue":"#191970", "mintcream":"#f5fffa", "mistyrose":"#ffe4e1", "moccasin":"#ffe4b5", "navajowhite":"#ffdead", 
+	"navy":"#000080", "oldlace":"#fdf5e6", "olive":"#808000", "olivedrab":"#6b8e23", "orange":"#ffa500", "orangered":"#ff4500", 
+	"orchid":"#da70d6", "palegoldenrod":"#eee8aa", "palegreen":"#98fb98", "paleturquoise":"#afeeee", "palevioletred":"#d87093", 
+	"papayawhip":"#ffefd5", "peachpuff":"#ffdab9", "peru":"#cd853f", "pink":"#ffc0cb", "plum":"#dda0dd", "powderblue":"#b0e0e6", 
+	"purple":"#800080", "rebeccapurple":"#663399", "red":"#ff0000", "rosybrown":"#bc8f8f", "royalblue":"#4169e1", "saddlebrown":"#8b4513", 
+	"salmon":"#fa8072", "sandybrown":"#f4a460", "seagreen":"#2e8b57", "seashell":"#fff5ee", "sienna":"#a0522d", "silver":"#c0c0c0", 
+	"skyblue":"#87ceeb", "slateblue":"#6a5acd", "slategray":"#708090", "snow":"#fffafa", "springgreen":"#00ff7f", "steelblue":"#4682b4",
+	"tan":"#d2b48c", "teal":"#008080", "thistle":"#d8bfd8", "tomato":"#ff6347", "turquoise":"#40e0d0", "violet":"#ee82ee", 
+	"wheat":"#f5deb3", "white":"#ffffff", "whitesmoke":"#f5f5f5", "yellow":"#ffff00", "yellowgreen":"#9acd32"};
 
+	constructor(colorString) {
+		this.r = 0;
+		this.g = 0;
+		this.b = 0;
+
+		this.h = 0;
+		this.s = 0;
+		this.l = 0;
+
+		this.a = 1;
+
+		if(tk.isDefined(colorString))
+			this.parse(colorString);
+	}
+
+	static fromElementBackgroundColor(element) {
+		return new tk.Color((new tk.Element(element).getComputed("background-color")));
+	}
+
+	static fromElementColor(element) {
+		return new tk.Color((new tk.Element(element).getComputed("color")));
+	}
+
+	static isColor(color) {
+		if (color === "" || color === "inherit" || color === "transparent") 
+			return false;
+
+		// Test if color changes from the test color
+		var image = make("img");
+		image.style.color = "rgb(0, 0, 0)";
+		image.style.color = color;
+
+		if (image.style.color !== "rgb(0, 0, 0)") 
+			return true; 
+
+		// Test again to account for the previously used test color
+		image.style.color = "rgb(255, 255, 255)";
+		image.style.color = color;
+
+		return (image.style.color !== "rgb(255, 255, 255)");
+	}
+
+	equals(otherColor) 	{
+		if(	otherColor.h == this.h 
+			&& otherColor.s == this.s 
+			&& otherColor.l == this.l 
+			&& otherColor.a == this.a)
+			return true;
+
+		return false;
+	}
+
+	randomize()	{
+		this.fromRgba(tk.Random.integer(0,255),
+		tk.Random.integer(0,255),
+		tk.Random.integer(0,255),
+		(tk.Random.decimal(0,1)).toFixed(2));
+	}
+
+	randomizeOpaque() {
+		this.fromRgba(tk.Random.integer(0,255),
+		tk.Random.integer(0,255),
+		tk.Random.integer(0,255),
+		1);
+	}
+
+	clone()	{
+		var newColor = new tk.Color();
+		newColor.fromHsla(this.h,this.s,this.l,this.a);
+		return newColor;
+	}
+
+	fromRgba(r, g, b, a) {
+		var hsl = this.rgbToHsl(r, g, b);
+		this.r = r;
+		this.g = g;
+		this.b = b;
+
+		this.h = hsl[0];
+		this.s = hsl[1];
+		this.l = hsl[2];
+
+		this.a = a;
+	}
+
+	fromHsla(h, s, l, a) {
+		var rgb = this.hslToRgb(h, s, l);
+		this.r = rgb[0];
+		this.g = rgb[1];
+		this.b = rgb[2];
+
+		this.h = h;
+		this.s = s;
+		this.l = l;
+
+		this.a = a;
+	}
+
+	fromHex(hex) {
+		var rgb = this.hexToRgb(hex);
+		this.fromRgba(rgb[0], rgb[1], rgb[2], this.a);
+	}
+
+	parse(colorString) {
+		if(colorString.startsWith("#")) {
+			this.fromHex(colorString);
+		} else if (tk.isDefined(tk.Color._named[colorString])){
+			this.fromHex(tk.Color._named[colorString]);
+		} else if (colorString.startsWith("hsl")) {
+			var trimmed = colorString.replace("hsla(","");
+			trimmed = trimmed.replace("hsl(","");
+			trimmed = trimmed.replace(")","");
+			trimmed = trimmed.replace("'","");
+			trimmed = trimmed.replace("\"","");
+			var chunks = trimmed.split(",");
+
+			if (chunks.length == 3) // hsl
+				this.fromHsla(parseInt(chunks[0]),parseInt(chunks[1]),parseInt(chunks[2]),1);
+			else if (chunks.length == 4) //hsla 
+				this.fromHsla(parseInt(chunks[0]),parseInt(chunks[1]),parseInt(chunks[2]),parseFloat(chunks[3]),1);
+		} else if (colorString.startsWith("rgb")) {
+			var trimmed = colorString.replace("rgba(","");
+			trimmed = trimmed.replace("rgb(","");
+			trimmed = trimmed.replace(")","");
+			trimmed = trimmed.replace("'","");
+			trimmed = trimmed.replace("\"","");
+			var chunks = trimmed.split(",");
+
+			if (chunks.length == 3) // rgb
+				this.fromRgba(parseInt(chunks[0]), parseInt(chunks[1]), parseInt(chunks[2]), 1);
+			else if (chunks.length == 4) //rgba
+				this.fromRgba(parseInt(chunks[0]), parseInt(chunks[1]), parseInt(chunks[2]), parseFloat(chunks[3]), 1);
+
+		} else {
+			this.fromRgba(0,0,0,0);
+		}
+	}
+	
+	asHslaString() {
+		return "hsla(" + this.h.toFixed(0) + ", " + this.s.toFixed(0) + "%, " + this.l.toFixed(0) + "%, " + this.a.toFixed(2) + ")";
+	}
+
+	asRgbaString() {
+		return "rgba(" + this.r.toFixed(0) + ", " + this.g.toFixed(0) + ", " + this.b.toFixed(0) + ", " + this.a.toFixed(2) + ")";
+	}
+	
+	asHexString()	{
+		return this.rgbToHex(this.r, this.g, this.b);
+	}
+	
+	hslToRgb(h, s, l) {
+		var r, g, b;
+
+		if(h>0) h /= 360;
+		if(s>0) s /= 100;
+		if(l>0) l /= 100;
+
+		if(s == 0) {
+			r = g = b = l; // achromatic
+		} else {
+			var hue2rgb = function hue2rgb(p, q, t) {
+				if(t < 0) t += 1;
+				if(t > 1) t -= 1;
+				if(t < 1/6) return p + (q - p) * 6 * t;
+				if(t < 1/2) return q;
+				if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+				return p;
+			}
+
+			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			var p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1/3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3);
+		}
+
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+	}
+	
+	rgbToHsl(r, g, b) {
+		r /= 255, g /= 255, b /= 255;
+		var max = Math.max(r, g, b), min = Math.min(r, g, b);
+		var h, s, l = (max + min) / 2;
+
+		if(max == min) {
+			h = s = 0; // achromatic
+		} else {
+			var d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch(max) {
+				case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+				case g: h = (b - r) / d + 2; break;
+				case b: h = (r - g) / d + 4; break;
+			}
+			h /= 6;
+		}
+
+		return [h*360, s*100, l*100];
+	}
+	
+	hexToHsl(hex)	{
+		var rgb = this.hexToRgb(hex);
+		return this.rgbToHsl(rgb[0], rgb[1], rgb[2]);
+	}
+	
+	hexToRgb(hex)	{
+		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+			return r + r + g + g + b + b;
+		});
+
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+	}
+	
+	hslToHex(h,s,l) {
+		var rgb = this.hslToRgb(h,s,l);
+		return this.rgbToHex(rgb[0], rgb[1], rgb[2]);
+	}
+	
+	rgbToHex(r,g,b) {
+		return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	}
+	
+	lighter(factor) {
+		var c = this.clone();
+		var newl = (factor * c.l) + c.l;
+		
+		if (newl > 100)
+			newl = 100;
+		else if (newl < 0)
+			newl = 0;
+
+		c.fromHsla(c.h, c.s, newl, c.a);
+		return c;
+	}
+	
+	getLuminance() {
+		var a = [this.r, this.g, this.b].map((v) => {
+			v /= 255;
+			return v <= 0.03928
+				? v / 12.92
+				: Math.pow( (v + 0.055) / 1.055, 2.4 );
+		});
+
+		return (a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722) * this.a;
+	}
+
+	contrastWith(color) {
+		return (color.getLuminance() + 0.05) / (this.getLuminance() + 0.05);
+	}
+	
+	isDark() {
+		return this.contrastWith(new tk.Color("white")) > 1.5;
+	}
+	
+	isLight() {
+		return (!this.isDark());
+	}
+	
+	isGray() {
+		return (tkArray.areElementsEqual([this.r,this.g,this.b]));
+	}
+
+	invert() {
+		
+	}
 }
 
 tk.Element = class {
@@ -113,8 +434,6 @@ tk.Element = class {
 
 			if (tk.isDefined(options.attributes)) {
 				var attributeNames = Object.keys(options.attributes);
-				console.log(attributeNames)
-
 				attributeNames.forEach(attributeName => 
 					this._base.setAttribute(attributeName, options.attributes[attributeName]));
 			}	
