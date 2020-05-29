35e3a4881a7e9cb3b17c9234a30f1aeee3cb6d93
Ian Martinez <ianmartinez97@live.com>
2020-05-28 23:05:05 -0500

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,40 @@
+/**
+ * ::: core.js :::
+ *  
+ * === TkDocument === 
+ * --- Scripts ---
+ *  - Get the tamarack/*.js files used in the document as an array
+      TkDocument.tamarackScripts { get; }
+ *  - Get a script {scriptName}.js by name
+ *      TkDocument.getScript("scriptName");
+ *  - Add a script to the DOM
+ *      TkDocument.addScript(scriptName);
+ *  - Remove a script from the DOM
+ *      TkDocument.removeScript(scriptName);
+ * 
+ * --- Styles ---
+ *  - Get the tamarack/*.css files used in the document as an array
+ *      TkDocument.tamarackStyles { get; }
+ *  - Get a style {styleName}.js by name
+ *      TkDocument.getStyle("styleName");
+ *  - Add a style to the DOM
+ *      TkDocument.addStyle(styleName);
+ *  - Remove a style from the DOM
+ *      TkDocument.removeStyle(styleName);
+ */
+
+ /**
+  * ::: icons.js :::
+  */
+
+  class TkIconSet {
+      
+      constructor(iconFolder) {
+
+      }
+
+      getIcon(iconName) {
+
+      }
+
+  }
