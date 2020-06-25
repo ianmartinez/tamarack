e9977b645ace184f306890be2705bfd75e37e8bf
Ian Martinez <ianmartinez97@live.com>
2020-06-24 22:37:24 -0500

    Update infiniteScroller.js

--- src/infiniteScroller.js
+++ src/infiniteScroller.js
@@ -263,6 +263,7 @@ class TkInfiniteScroller extends TkPanel {
             this._cachedItems[i].top = -1;
             this.add(widget);
             this._cachedItems[i].widget = widget;
+            this.onResize();
         }
 
         // Remove all unused widgets
