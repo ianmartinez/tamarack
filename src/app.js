3d0a960d7db031e1f4d2edf97d20aa10bb0078f5
Ian Martinez <ianmartinez97@live.com>
2020-07-14 22:24:58 -0500

    Basic out-of-the-box dark mode support for tkapps

--- src/app.js
+++ src/app.js
@@ -1,5 +1,77 @@
 /*  Handles views managing toolbars, menus, and icons */
 
+
+/**
+ * An enum representing the system
+ * the app is running in.
+ * @enum {String}
+ */
+const TkAppType = {
+    WEB: "web",
+    ELECTRON: "electron",
+    CORDOVA: "cordova"
+};
+
+class TkApp {
+
+
+    /**
+     * Set up attributes on root <html> node to 
+     * allow for CSS selectors to be environment-
+     * specific.
+     * 
+     * Attributes are the same as the string value
+     * returned by TkEnviroment.system.
+     * 
+     * Also adds [tkapp] attribute.
+     */
+    static init() {
+        TkDocument.whenLoaded(() => {
+            let currentSystem = TkApp.target;
+            let htmlNode = document.querySelector("html");
+            htmlNode.setAttribute(currentSystem, "true");
+            htmlNode.setAttribute("tkapp", "true");
+        });
+    }
+
+    static get target() {
+        if (TkApp.isElectron)
+            return TkAppType.ELECTRON;
+        else if (TkApp.isCordova)
+            return TkApp.CORDOVA;
+        else
+            return TkApp.WEB;
+    }
+
+    static get isElectron() {
+        // Renderer process
+        if (typeof window !== "undefined" && typeof window.process === "object" && window.process.type === "renderer") {
+            return true;
+        }
+
+        // Main process
+        if (typeof process !== "undefined" && typeof process.versions === "object" && !!process.versions.electron) {
+            return true;
+        }
+
+        // Detect the user agent when the `nodeIntegration` option is set to true
+        if (typeof navigator === "object" && typeof navigator.userAgent === "string" && navigator.userAgent.indexOf("Electron") >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // TODO: Detect Cordova
+    static get isCordova() {
+        return false;
+    }
+
+    static get isWeb() {
+        return !this.isCordova && !this.isElectron;
+    }
+}
+
 class TkToolbar extends TkStack {
 
     constructor(options = {}) {
