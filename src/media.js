35a77042afdcd3791d0f8dccbfa55dec92c71348
Ian Martinez <ianmartinez97@live.com>
2020-07-18 22:26:20 -0500

    Add basic media player views

--- src/media.js
+++ src/media.js
@@ -1,14 +1,59 @@
 /* TODO */
 class TkMedia extends TkView {
-    
+
+    constructor(tag, options = {}) {
+        options.tag = tag;
+        super(options);
+
+        this.showControls = options.showControls ?? true;
+
+        if (options.source !== undefined)
+            this.source = options.source;
+
+        if (options.autoplay !== undefined)
+            this.autoplay = options.autoplay;
+    }
+
+    get source() {
+        return this.e.src;
+    }
+
+    set source(value) {
+        this.e.src = value;
+    }
+
+    get showControls() {
+        return this.hasAttribute("controls");
+    }
+
+    set showControls(value) {
+        this.attributeIf(value, "controls");
+    }
+
+    get autoplay() {
+        return this.hasAttribute("autoplay");
+    }
+
+    set autoplay(value) {
+        this.attributeIf(value, "autoplay");
+    }
+
 }
 
 /* TODO */
 class TkAudio extends TkMedia {
 
+    constructor(options = {}) {
+        super("audio", options);
+    }
+
 }
 
 /* TODO */
 class TkVideo extends TkMedia {
+
+    constructor(options = {}) {
+        super("video", options);
+    }
 
 }
